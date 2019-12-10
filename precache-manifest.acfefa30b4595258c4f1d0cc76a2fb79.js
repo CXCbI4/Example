de35ca801fce533e340351a94e8acb73495bb36f
CXCbI4 <aleksei6620@gmail.com>
2019-12-10 14:09:54 +0300

    Updates

---
+++ precache-manifest.acfefa30b4595258c4f1d0cc76a2fb79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "33c8c0feaa9eb28da6aa",
+    "url": "./static/css/main.2961d87e.chunk.css"
+  },
+  {
+    "revision": "33c8c0feaa9eb28da6aa",
+    "url": "./static/js/main.08da7f7d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "46531aaf3c5902a96b91",
+    "url": "./static/css/2.d9eb256e.chunk.css"
+  },
+  {
+    "revision": "46531aaf3c5902a96b91",
+    "url": "./static/js/2.98ecb967.chunk.js"
+  },
+  {
+    "revision": "ceadf317baab79c73472d7b4e3f0f9a4",
+    "url": "./index.html"
+  }
+];
